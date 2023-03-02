fbb8763c6064b413071a7fa9ed9050485cdd4c6f
Jay Y <alforoanjay@gmail.com>
2023-03-02 07:18:39 +0000

    Completed Combine Arrays Lab

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class CombineArrays {
     /**
      * Combine two arrays into a single array and return it.
@@ -8,7 +8,15 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int[] arr3 = new int[arr1.length + arr2.length];
 
-        return null;
+        for (int i = 0; i < arr1.length; i++) {
+            arr3[i] = arr1[i];
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            arr3[arr1.length + i] = arr2[i];
+        }
+        return arr3;
     }
 }
